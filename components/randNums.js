f88da0fddd3c1468350bb09ce5968ac4dd5b9cdb
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-02-26 09:49:19 +0100

    Add hatosLotto function

--- components/randNums.js
+++ components/randNums.js
@@ -25,3 +25,17 @@ export const otosLotto = () => {
 
   return numbersArr.sort((a, b) => a - b);
 }
+
+export const hatosLotto = () => {
+  const numbersArr = [];
+
+  while (numbersArr.length < 6) {
+    const randNum = Math.floor(Math.random() * 45) + 1;
+
+    if(!numbersArr.includes(randNum)) {
+      numbersArr.push(randNum);
+    }
+  }
+
+  return numbersArr.sort((a, b) => a - b);
+}
