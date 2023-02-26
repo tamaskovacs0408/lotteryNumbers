cc48035ece7b4f809c061b3e5c83f8a1cf32e334
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-02-26 09:46:55 +0100

    Add function for otosLotto

--- components/randNums.js
+++ components/randNums.js
@@ -1,4 +1,4 @@
-const randNums = () => {
+export const skandinavLotto = () => {
   const numbersArr = [];
 
   while (numbersArr.length < 7) {
@@ -12,4 +12,16 @@ const randNums = () => {
   return numbersArr.sort((a, b) => a - b);
 };
 
-export default randNums;
+export const otosLotto = () => {
+  const numbersArr = [];
+
+  while (numbersArr.length < 5) {
+    const randNum = Math.floor(Math.random() * 90) + 1;
+
+    if (!numbersArr.includes(randNum)) {
+      numbersArr.push(randNum);
+    }
+  }
+
+  return numbersArr.sort((a, b) => a - b);
+}
