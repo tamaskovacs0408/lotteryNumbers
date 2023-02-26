3dd770340aa3698b08c3c6827da0a6cb59f7bf1d
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-02-26 11:36:04 +0100

    Update list styling

--- App.js
+++ App.js
@@ -7,13 +7,12 @@ export default function App() {
   const date = new Date();
 
   function handlePress() {
-    const numbers = skandinavLotto()
-    const currentDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
-    console.log(currentDate)
-
+    const numbers = skandinavLotto();
+    const currentDate =
+      date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
     setWinningNumbers((currentNumbers) => [
       ...currentNumbers,
-      { nums: numbers.join(',') , date: currentDate},
+      { nums: numbers.join(","), date: currentDate },
     ]);
   }
 
@@ -24,14 +23,17 @@ export default function App() {
         <Text style={styles.generatorBtnTxt}>Generálás</Text>
       </Pressable>
       <View style={styles.numbersContainer}>
-        <FlatList data={winningNumbers} renderItem={(numbersData) => {
-          return (
+        <FlatList
+          data={winningNumbers}
+          renderItem={(numbersData) => {
+            return (
               <View style={styles.numbersView}>
                 <Text style={styles.generateDate}>{numbersData.item.date}</Text>
                 <Text style={styles.winNumbers}>{numbersData.item.nums}</Text>
               </View>
-          )
-        }} />
+            );
+          }}
+        />
       </View>
     </View>
   );
@@ -43,7 +45,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#93D14F",
     alignItems: "center",
     justifyContent: "center",
-    paddingTop: 30,
+    paddingTop: 50,
   },
   mainTitle: {
     fontSize: 40,
@@ -70,18 +72,29 @@ const styles = StyleSheet.create({
   numbersContainer: {
     flex: 3,
     backgroundColor: "#93D14F",
+    justifyContent: "center",
+    alignItems: "center",
   },
   numbersView: {
-    borderColor: "#ffffff",
+    borderColor: "#FAFAFF",
     borderRadius: 10,
     borderWidth: 2,
     flexDirection: "row",
+    width: "80%",
+    marginTop: 8,
+    marginBottom: 8,
+    marginLeft: "auto",
+    marginRight: "auto",
+    padding: 10,
+    textAlign: "center",
+    justifyContent: "space-between",
   },
   generateDate: {
-    color: "#ffffff",
-    font
+    color: "#FAFAFF",
+    fontSize: 16,
   },
   winNumbers: {
-    color: "#ffffff"
-  }
+    color: "#FAFAFF",
+    fontSize: 20,
+  },
 });
