7c520c80f5cfaf33fec5ec7c41609ea0f7f5c3c0
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-02-26 11:22:59 +0100

    Update app

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
 import { useState } from "react";
 import { skandinavLotto } from "./components/randNums";
-import { StyleSheet, Text, View, Pressable, Button } from "react-native";
+import { StyleSheet, Text, View, Pressable, FlatList } from "react-native";
 
 export default function App() {
   const [winningNumbers, setWinningNumbers] = useState([]);
+  const date = new Date();
 
   function handlePress() {
-    console.log(skandinavLotto());
+    const numbers = skandinavLotto()
+    const currentDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+    console.log(currentDate)
+
+    setWinningNumbers((currentNumbers) => [
+      ...currentNumbers,
+      { nums: numbers.join(',') , date: currentDate},
+    ]);
   }
 
   return (
     <View style={styles.container}>
-        <Text style={styles.mainTitle}>Lottó szám generátor</Text>
-        <Pressable onPress={handlePress} style={styles.generatorBtn}>
-          <Text style={styles.generatorBtnTxt}>Generálás</Text>
-        </Pressable>
-      <View style={styles.numbersContainer}></View>
+      <Text style={styles.mainTitle}>Lottó szám generátor</Text>
+      <Pressable onPress={handlePress} style={styles.generatorBtn}>
+        <Text style={styles.generatorBtnTxt}>Generálás</Text>
+      </Pressable>
+      <View style={styles.numbersContainer}>
+        <FlatList data={winningNumbers} renderItem={(numbersData) => {
+          return (
+              <View style={styles.numbersView}>
+                <Text style={styles.generateDate}>{numbersData.item.date}</Text>
+                <Text style={styles.winNumbers}>{numbersData.item.nums}</Text>
+              </View>
+          )
+        }} />
+      </View>
     </View>
   );
 }
@@ -26,7 +43,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#93D14F",
     alignItems: "center",
     justifyContent: "center",
-    paddingTop: 30
+    paddingTop: 30,
   },
   mainTitle: {
     fontSize: 40,
@@ -54,4 +71,17 @@ const styles = StyleSheet.create({
     flex: 3,
     backgroundColor: "#93D14F",
   },
+  numbersView: {
+    borderColor: "#ffffff",
+    borderRadius: 10,
+    borderWidth: 2,
+    flexDirection: "row",
+  },
+  generateDate: {
+    color: "#ffffff",
+    font
+  },
+  winNumbers: {
+    color: "#ffffff"
+  }
 });
